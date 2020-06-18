010b101c82f8386654f9f1efc59f89b687e09fb5
ashish1469 <63723389+ashish1469@users.noreply.github.com>
2020-06-18 07:48:29 +0530

    Code

--- task.tf
+++ task.tf
@@ -3,7 +3,7 @@ provider "aws" {
   profile    = "ashish"
 }
 resource "aws_key_pair" "deployer" {
-  key_name   = "mykey1111"
+  key_name   = "your key name"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
 
@@ -134,7 +134,8 @@ resource "aws_instance" "myinstance1" {
   depends_on = [ aws_cloudfront_distribution.s3_distribution ]
   ami           = "ami-052c08d70def0ac62"
   instance_type = "t2.micro"
-  key_name = "mykey1111"
+  key_name = "mykey1
+"
   security_groups = [ "launch-wizard-2" ]
 
   connection {
